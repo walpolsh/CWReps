59b030f3d18d572cceeb927bbd9c1664b1b4de26
Paul Walsh <twentytwentyk@live.com>
2019-10-04 12:20:05 -0400

    sequence sum

--- src/7KYU/sequenceSum.js
+++ src/7KYU/sequenceSum.js
@@ -1,8 +1,17 @@
-export const sequenceSum = (begin, end, step) => {
-  let res = begin;
-  for (let i = begin; i < end; i++) {
-    res += step;
-  }
+// const sequenceSum = (begin, end, step) => {
+//   let cur = begin;
+//   let res = [begin];
 
-  return res;
-};
+//   while (cur < end) {
+//     res.push((cur += step));
+//   }
+//   const sum = (a, c) => a + c;
+
+//   return res.filter(x => x <= end).reduce(sum, 0);
+// };
+const sequenceSum = (b, e, s) => (b > e ? 0 : b + sequenceSum(b + s, e, s));
+
+console.log(sequenceSum(2, 6, 2));
+
+console.log(sequenceSum(1, 5, 1));
+console.log(sequenceSum(1, 5, 3));
