1e4df468751abb39a6e20a919a0a35e3fe4bcd39
Paul Walsh <twentytwentyk@live.com>
2019-11-08 01:19:34 -0500

    one away

--- src/CTCI/Ch1/Ch1.test.js
+++ src/CTCI/Ch1/Ch1.test.js
@@ -1,6 +1,7 @@
 import { checkPermutation, checkPermutation2 } from "./checkPermutation";
 import { isUnique, isUnique2 } from "./isUnique";
 import { URLfy } from "./URLfy";
+import { oneAway } from "./oneAway";
 function expectToBe(fn, input, output) {
   return it(`expect ${fn.name}('${input}') to be ${"" + output})}`, () =>
     expect(fn(input)).toBe(output));
@@ -14,11 +15,11 @@ expectToBe(checkPermutation, ["cat", "tac"], true);
 expectToBe(checkPermutation, ["coat", "taco"], true);
 expectToBe(checkPermutation, ["pen", "bug"], false);
 expectToBe(checkPermutation, ["knife", "bag"], false);
-expectToBe(checkPermutation2, ["aa", "aa"], true);
-expectToBe(checkPermutation2, ["cat", "tac"], true);
-expectToBe(checkPermutation2, ["coat", "taco"], true);
-expectToBe(checkPermutation2, ["pen", "bug"], false);
-expectToBe(checkPermutation2, ["knife", "bag"], false);
 
 expectToBe(URLfy, "Mr. Paul Walsh   ", "Mr.%20Paul%20Walsh");
 expectToBe(URLfy, " Mr.  Paul   Walsh   ", "Mr.%20Paul%20Walsh");
+
+expectToBe(oneAway, ["pale", "ple"], true);
+expectToBe(oneAway, ["pales", "pale"], true);
+expectToBe(oneAway, ["pale", "bale"], true);
+expectToBe(oneAway, ["pale", "bake"], false);
---
+++ src/CTCI/Ch1/oneAway.js
@@ -0,0 +1,62 @@
+export const oneAway = string1 => {
+  // insert a char for str1 -> remove a char for str2
+  const checkOneMissing = function(first = first1[0], second = first[1]) {
+    if (first.length !== second.length - 1) {
+      return false;
+    } else {
+      var mulligan = false;
+      var fP = 0; // first Pointer
+      var sP = 0; // second Pointer
+      while (fP < first.length) {
+        if (first[fP] !== second[sP]) {
+          if (mulligan) {
+            return false;
+          } else {
+            mulligan = true;
+            sP++; // second length is longer
+          }
+        } else {
+          fP++;
+          sP++;
+        }
+      }
+      return true;
+    }
+  };
+
+  const checkOneDiff = (first, second) => {
+    if (first.length !== second.length) {
+      return false;
+    } else {
+      var mulligan = false;
+      var fP = 0; // first Pointer
+      var sP = 0; // second Pointer
+      while (fP < first.length) {
+        if (first[fP] !== second[sP]) {
+          if (mulligan) {
+            return false; // more than one mismatch
+          } else {
+            mulligan = true; // use up mulligan
+          }
+        }
+        fP++;
+        sP++;
+      }
+      return true;
+    }
+  };
+
+  const res1 = checkOneMissing(string1[0], string1[1]);
+  const res2 = checkOneMissing(string1[1], string1[0]);
+  const res3 = checkOneDiff(string1[0], string1[1]);
+
+  // insert a char for str1 -> remove a char for str2
+  // check one diff
+
+  // console log checks
+  // console.log(string1, string2, 'checkMiss', checkOneMissing(string1, string2));
+  // console.log(string2, string1, 'checkMiss', checkOneMissing(string2, string1));
+  // console.log(string1, string2, 'checkDiff', checkOneDiff(string1, string2));
+
+  return res1 || res2 || res3;
+};
