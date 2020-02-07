3052bf31de69e99645618e6aa84778767ac02eb8
Paul Walsh <twentytwentyk@live.com>
2020-02-07 02:06:12 -0500

    insert

--- src/Algos/linkedList.js
+++ src/Algos/linkedList.js
@@ -23,12 +23,48 @@
 //     this.length++;
 //   }
 // }
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+// class LinkedList {
+//   constructor(value) {
+//     this.head = {
+//       value: value,
+//       next: null
+//     };
+//     this.tail = this.head;
+//     this.length = 1;
+//   }
+//   append(value) {
+//     const newNode = new Node(value);
+//     //the new node is appended to the tail.
+//     this.tail.next = newNode;
+//     // the new tail is the new node
+//     this.tail = newNode;
+//     //increase the length by one
+//     this.length++;
+//   }
+//   prepend(value) {
+//     const newNode = new Node(value);
+//     // the new node's next value is the head
+//     newNode.next = this.head;
+//     // the new head is the new node.
+//     this.head = newNode;
+//     //increase the length of the linked list by one
+//     this.length++;
+//   }
+// }
+
 class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
   }
 }
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -40,21 +76,44 @@ class LinkedList {
   }
   append(value) {
     const newNode = new Node(value);
-    //the new node is appended to the tail.
     this.tail.next = newNode;
-    // the new tail is the new node
     this.tail = newNode;
-    //increase the length by one
     this.length++;
   }
+
   prepend(value) {
     const newNode = new Node(value);
-    // the new node's next value is the head
     newNode.next = this.head;
-    // the new head is the new node.
     this.head = newNode;
-    //increase the length of the linked list by one
     this.length++;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+  }
+  traverseToIndex(index) {
+    let currentNode = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    let leadingNode = this.traverseToIndex(index - 1);
+    let holdingPointer = leadingNode.next;
+    leadingNode.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
   }
 }
 
@@ -62,4 +121,7 @@ const myLinkedList = new LinkedList(2);
 myLinkedList.append(3);
 myLinkedList.append(4);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+myLinkedList.insert(1, "hello");
+myLinkedList.insert(9999, "goodbye");
+
+myLinkedList.printList();
