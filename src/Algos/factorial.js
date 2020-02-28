7e6d1f2a1cee0e90bd86d519b35edfd0455ef63d
walpolsh <twentytwentyk@live.com>
2020-02-28 06:47:17 -0500

    memo factorial

--- src/Algos/factorial.js
+++ src/Algos/factorial.js
@@ -6,3 +6,19 @@ const factorial = n => {
   return arr.reduce((a, c) => a * c);
 };
 factorial(5);
+
+const factorial2 = n => {
+  if (n === 1) return n;
+  return n * factorial2(n - 1);
+};
+factorial2(5);
+
+let memo = {};
+const factorial3 = n => {
+  if (n in memo) return memo[n];
+  if (n === 1) return n;
+  let x = n * factorial3(n - 1);
+  memo[n] = x;
+  return x;
+};
+factorial3(5);
